62f26df61f9c55bf631518770dc954318f0dcd4a
Rae-Lyn Morikawa <rrmorikawa@gmail.com>
2018-05-16 15:56:37 -0700

    gitignore babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,12 @@ module.exports = {
   output: {
     path: './build',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{ 
+      test: /\.js$/, 
+      exclude: /node_modules/, 
+      loader: "babel-loader" 
+    }]
   }
 };
